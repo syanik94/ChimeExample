ac75291182e60d7133062ef2a92fc736f25b2bed
Yanik <syanik@yahoo.com>
2020-09-13 18:18:00 -0500

    Remove SceneDelegate boilerplate methods

---
+++ ChimeExample/Modules/Login/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  ChimeExample
+//
+//  Created by Yanik Simpson on 9/13/20.
+//  Copyright © 2020 Yanik Simpson. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+}
---
+++ ChimeExample/Supporting Files/SceneDelegate.swift
@@ -0,0 +1,32 @@
+//
+//  SceneDelegate.swift
+//  ChimeExample
+//
+//  Created by Yanik Simpson on 9/9/20.
+//  Copyright © 2020 Yanik Simpson. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+
+        // Use a UIHostingController as window root view controller.
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = HomeViewController()
+            self.window = window
+            window.makeKeyAndVisible()
+        }
+    }
+
+}
+
